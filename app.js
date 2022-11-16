f4ab45f9eeeda93d23b098901636e19394bee956
unknown <saurabh.s@spintly.com>
2022-11-16 18:26:37 +0530

    Commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.listen(port, () => {
 /* you can pass your auth middleware from here if needed */
 Routes.TaskRoutes(app);
 
-app.get("/tasks-app/v1/tasks", (_, res) => {
+app.get("/https://source-tasks-app.herokuapp.com/", (_, res) => {
   console.log(_.params);
   res.send({
     type: "success",
